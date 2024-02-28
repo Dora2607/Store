17a74c6a765c7e7dce86c0f89e13a012699ae64e
Dora <dora.sparagno@gmail.com>
2024-02-28 18:23:19 +0100

    feat: Added removeFromCart and removeQuantity methods to CartService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from './models/cart.model';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   template: `
-   <app-headers></app-headers>
+   <app-headers [cart]="cart"></app-headers>
    <router-outlet> </router-outlet>
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'Store';
+export class AppComponent implements OnInit {
+  cart: Cart = { items: []};
+
+  constructor(private cartService: CartService){}
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart)=>{
+      this.cart = _cart;
+    })
+  }
 }
